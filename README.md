9f0fdd74fdf4b1ac3106e3762fe8a2dc99bf6557
Christopher Wallis <supercryptonite@gmail.com>
2016-09-16 08:04:50 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Webhook Deploy for Node
+
+HTTP server that runs deployment scripts when a GET request is sent.  Useful for automatically deploying from GitHub webhooks. 
+Supports multiple sites or multiple deployment strategies for each site.
+
---
+++ server.js
@@ -0,0 +1,80 @@
+var http = require('http');
+const execSync = require('child_process').execSync;
+const deployOptions = require('./deploy.json');
+const PORT = deployOptions.port; 
+
+function handleRequest(req, resp) {
+	const site = findSite(req.url);
+
+	if (site === undefined) {
+		responseError(resp, 404, '404: Not found');
+		return;
+	}
+
+	resp.writeHead(200, { "Content-Type": "text/html" });
+
+	try {
+		const output = deploy(site);
+		output.forEach(o => {
+			resp.write('<p>');
+			resp.write(o.toString().replace(/\n/ig, '<br/>'));
+			resp.write('</p>');
+		});
+	} catch (err) {
+		responseError(resp, 500, err);
+		return;
+	}
+
+    resp.end();
+}
+
+function responseError(resp, code, msg) {
+	resp.writeHead(code, { "Content-Type": "text/plain" });
+	resp.write(msg)
+	resp.end();
+}
+
+function findSite(url) {
+	const sites = deployOptions.sites;
+	for (let i = 0, len = sites.length; i < len; i++) {
+		const site = sites[i];
+		if (site.url.toLowerCase() === url.toLowerCase()) {
+			return site;
+		}
+	}
+	return undefined;
+}
+
+
+function deploy(site) {
+	const url = site.url;
+	const workingDir = site.workingDir;	
+	const scripts = site.scripts || [];
+
+	const processOptions = {};	
+	if (workingDir) {
+		processOptions.cwd = workingDir;
+	}
+
+	const output = [];
+	let hasError = false;
+	try {
+		// print out the url of the deployment
+		output.push(execSync(`echo "deploying: ${url}"`, processOptions));
+
+		scripts.forEach(script => {
+			output.push(execSync(script, processOptions));			
+		})
+	} catch (err) {
+		hasError = true;		
+		output.push(err);
+	}
+
+	return output;
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+server.listen(PORT, function(){
+    console.log("Server listening on port: %s", PORT);
+});
